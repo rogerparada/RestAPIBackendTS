5070022bf3d579cfd1440c5a79f0a1133a838f51
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2024-09-07 17:51:53 +0200

    Delete extra files

--- dist/models/Product.model.d.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from "sequelize-typescript";
-declare class Product extends Model {
-    name: string;
-    price: number;
-    availability: boolean;
-}
-export default Product;
--- dist/server.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function connectDB(): Promise<void>;
-declare const server: import("express-serve-static-core").Express;
-export default server;
